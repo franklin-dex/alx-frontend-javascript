62d7b986cabd31bdc34ed25fa0c645af46c1c829
franklin-dex <odinakachukwufranklin0@gmail.com>
2024-07-31 13:45:01 -0700

    add an interface for a student in main.ts file

--- 0x04-TypeScript/task_0/js/main.ts
+++ 0x04-TypeScript/task_0/js/main.ts
@@ -8,15 +8,41 @@ interface Student {
 const Student1: Student = {
     firstName: 'Frank',
     lastName: 'Excel',
-    age: '22',
+    age: 22,
     location: 'Lagos',
 }
 
 const Student2: Student = {
     firstName: 'Anita',
     lastName: 'Jessica',
-    age: '20',
+    age: 20,
     location: 'Owerri',
 }
 
 const studentsList: Student[] = [Student1, Student2];
+
+const table = document.createElement('table');
+const tbody = document.createElement('tbody');
+
+table.style.background = "black";
+table.appendChild(tbody);
+
+studentsList.forEach((student: Student): void => {
+  const row = document.createElement('tr');
+  const nameCell = document.createElement('td');
+  const locationCell = document.createElement('td');
+
+  nameCell.textContent = student.firstName;
+  locationCell.textContent = student.location;
+
+  nameCell.style.border = "1px solid pink";
+  locationCell.style.border = "1px solid pink";
+  nameCell.style.padding = "5px";
+  locationCell.style.padding = "5px";
+
+  row.appendChild(nameCell);
+  row.appendChild(locationCell);
+  tbody.appendChild(row);
+});
+
+document.body.appendChild(table);
