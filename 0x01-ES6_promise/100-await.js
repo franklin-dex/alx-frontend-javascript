a49c3bc5a5bf76b498dbbddeee2ed5dd0398ebeb
franklin-dex <odinakachukwufranklin0@gmail.com>
2024-10-31 02:05:29 +0100

    add task 10  Await / Async

---
+++ 0x01-ES6_promise/100-await.js
@@ -0,0 +1,11 @@
+import { uploadPhoto, createUser } from './utils';
+
+export default async function asyncUploadUser() {
+  try {
+    const photo = await uploadPhoto('photo-profile-1');
+    const user = await createUser('Guillaume', 'Salva');
+    return { photo, user };
+  } catch {
+    return { photo: null, user: null };
+  }
+}
